cb0d324bfaa08969798686741a7053df78060c6d
OjasAT2105 <your_email@example.com>
2025-09-12 10:38:16 +0530

    Created new plugin and made a block with old way

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+	Plugin Name: Are You Paying Attention Quiz
+	Description: Give your readers a multiple choice question.
+	Version 1.0
+	Author: BSF
+	Author URI: https://www.youtube.com
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+class AreYouPayingAttention {
+	function __construct() {
+		add_action( 'enqueue_block_editor_assets', array( $this, 'adminAssets' ) );
+	}
+
+	function adminAssets() {
+		wp_enqueue_script( 'ournewblocktype', plugin_dir_url( __FILE__ ) . 'build/index.js', array( 'wp-blocks','wp-element' ) );
+	}
+}
+
+$areyoupayingattention = new AreYouPayingAttention();
---
+++ src/index.js
@@ -0,0 +1,78 @@
+wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
+  title: "Are You Paying Attention?",
+  icon: "smiley",
+  category: "common",
+  attributes: {
+    skyColor: { type: "string" },
+    grassColor: { type: "string" },
+  },
+  edit: function (props) {
+    function updateSkyColor(e) {
+      props.setAttributes({ skyColor: e.target.value });
+    }
+    function updateGrassColor(e) {
+      props.setAttributes({ grassColor: e.target.value });
+    }
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Sky Color"
+          value={props.attributes.skyColor}
+          onChange={updateSkyColor}
+        />
+        <input
+          type="text"
+          placeholder="Grass Color"
+          value={props.attributes.grassColor}
+          onChange={updateGrassColor}
+        />
+      </div>
+    );
+  },
+  save: function (props) {
+    return (
+      <p>
+        Today the sky is{" "}
+        <span className="skyColor">{props.attributes.skyColor}</span> and the
+        grass is
+        <span className="grassColor">{props.attributes.grassColor}</span>.
+      </p>
+    );
+  },
+  depricated: [
+    {
+      attributes: {
+        skyColor: { type: "string" },
+        grassColor: { type: "string" },
+      },
+
+      save: function (props) {
+        return (
+          <p>
+            Today the sky is{" "}
+            <span className="skyColor">{props.attributes.skyColor}</span> and
+            the grass is
+            <span className="grassColor">{props.attributes.grassColor}</span>.
+          </p>
+        );
+      },
+    },
+    {
+      attributes: {
+        skyColor: { type: "string" },
+        grassColor: { type: "string" },
+      },
+      save: function (props) {
+        return (
+          <p>
+            Today the sky is{" "}
+            <span className="skyColor">{props.attributes.skyColor}</span> and
+            the grass is
+            <span className="grassColor">{props.attributes.grassColor}</span>.
+          </p>
+        );
+      },
+    },
+  ],
+});
